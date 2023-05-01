aed5c7b5aca87ba5389a537314d73f8db33fae4d
Abu Bakar <abubakar.hasan.cse@gmail.com>
2023-05-01 17:25:46 +0600

    final checked completed before deploy on vercel

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const categories = require('./data/categories.json');
+const cors = require('cors')
+
 const news = require('./data/news.json');
+
+
+app.use(cors())
 
 app.get('/', (req, res) =>{
     res.send('Dragon is running')
@@ -14,6 +19,23 @@ app.get('/categories', (req, res) =>{
 
 app.get('/news', (req, res)=>{
     res.send(news);
+});
+
+app.get('/news/:id', (req, res)=>{
+    const id = req.params.id;
+    const selectedNews = news.find(n =>n._id===id);
+    res.send(selectedNews)
+})
+
+app.get('/categories/:id', (req, res) =>{
+    const id = Number(req.params.id);
+    if(id=== 0){
+        res.send(news);
+    }
+    else{
+        const categoryNews = news.filter(n => Number(n.category_id) === id);
+        res.send(categoryNews)
+    }
 })
 
 app.listen(port,()=>{
